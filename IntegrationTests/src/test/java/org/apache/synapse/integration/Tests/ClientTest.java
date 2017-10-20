08a88ee7c69f091307281eb42b812c7b18b04438
riyafa <riyafa.12@cse.mrt.ac.lk>
2017-10-20 13:02:37 +0530

    Added client test cases

--- IntegrationTests/src/test/java/org/apache/synapse/integration/Tests/ClientTest.java
+++ IntegrationTests/src/test/java/org/apache/synapse/integration/Tests/ClientTest.java
@@ -2,18 +2,23 @@ package org.apache.synapse.integration.Tests;
 
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import org.apache.synapse.integration.BaseTest;
 import org.apache.synapse.integration.utils.TestUtils;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.wso2.carbon.protocol.emulator.dsl.Emulator;
 import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientConfigBuilderContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientOperationBuilderContext;
 import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientRequestBuilderContext;
 import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientResponseBuilderContext;
 import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientResponseProcessorContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.RequestResponseCorrelation;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class ClientTest extends BaseTest {
     private String path = "/services/normal_server";
@@ -24,6 +29,14 @@ public class ClientTest extends BaseTest {
             "usedtocreatemarkuplanguagessuchasDocBook.\",\"GlossSeeAlso\":[\"GML\",\"XML\"]}," +
             "\"GlossSee\":\"markup\"}}}}}";
     private File plainFile = new File("100KB.txt");
+    private String processingPath = "/services/content_type";
+    private String xmlBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<note>\n" +
+            "  <to>Tove</to>\n" +
+            "  <from>Jani</from>\n" +
+            "  <heading>Reminder</heading>\n" +
+            "  <body>Don't forget me this weekend!</body>\n" +
+            "</note>";
 
     protected String getSynapseConfig() throws IOException {
 
@@ -77,4 +90,224 @@ public class ClientTest extends BaseTest {
     }
 
 
+    @Test
+    public void testClientSlowReading() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort())).withReadingDelay(3000)
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath(path)
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals(responseBody, response.getReceivedResponseContext().getResponseBody());
+        Assert.assertEquals(HttpHeaders.Values.APPLICATION_JSON,
+                            response.getReceivedResponse().headers().get(HttpHeaders.Names.CONTENT_TYPE));
+    }
+
+    @Test
+    public void testClientDisableKeepAlive() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort())).withKeepAlive(false)
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath(path)
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals(responseBody, response.getReceivedResponseContext().getResponseBody());
+        Assert.assertEquals(HttpHeaders.Values.APPLICATION_JSON,
+                            response.getReceivedResponse().headers().get(HttpHeaders.Names.CONTENT_TYPE));
+        Assert.assertNotEquals(response.getReceivedResponse().headers().get(HttpHeaders.Names.CONNECTION),
+                               HttpHeaders.Values.KEEP_ALIVE);
+        Assert.assertEquals(response.getReceivedResponse().headers().get(HttpHeaders.Names.CONNECTION),
+                            HttpHeaders.Values.CLOSE);
+    }
+
+    @Test
+    public void testDisableChunking() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath(path)
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals(responseBody, response.getReceivedResponseContext().getResponseBody());
+        Assert.assertEquals(HttpHeaders.Values.APPLICATION_JSON,
+                            response.getReceivedResponse().headers().get(HttpHeaders.Names.CONTENT_TYPE));
+    }
+
+    @Test
+    public void testDisconnectPartially() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                                .withPartialWriteConnectionDrop()
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath(path)
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+
+        Assert.assertNull(response);
+    }
+
+    @Test
+    public void testBurstRequests() {
+        for (int i = 0; i < 10; i++) {
+            HttpClientOperationBuilderContext httpClientOperationBuilderContext = Emulator.getHttpEmulator()
+                    .client()
+                    .given(
+                            HttpClientConfigBuilderContext.configure()
+                                    .host(getConfig().getSynapseServer().getHostname())
+                                    .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                    )
+                    .when(
+                            HttpClientRequestBuilderContext.request().withPath(path)
+                                    .withMethod(HttpMethod.POST).withBody(plainFile)
+                    )
+                    .then(
+                            HttpClientResponseBuilderContext.response().assertionIgnore()
+                    )
+                    .operation()
+                    .sendAsync();
+            List<RequestResponseCorrelation> responseCorrelations = httpClientOperationBuilderContext.shutdown();
+            Assert.assertEquals(responseBody, responseCorrelations.get(0).getReceivedResponse()
+                    .getReceivedResponseContext()
+                    .getResponseBody());
+            Assert.assertEquals(HttpHeaders.Values.APPLICATION_JSON, responseCorrelations.get(0).getReceivedResponse().getReceivedResponse()
+                    .headers().get(HttpHeaders.Names.CONTENT_TYPE));
+        }
+    }
+
+    @Test
+    public void testMalformedPayload() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath(processingPath)
+                                .withMethod(HttpMethod.POST).withBody("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                                                                              "<note>\n" +
+                                                                              "  <to>Tove<to>\n" +
+                                                                              "  <from>Jani</from>\n" +
+                                                                              "  <heading>Reminder</heading>\n" +
+                                                                              "  <body>Don't forget me this " +
+                                                                              "weekend!</body>\n" +
+                                                                              "</note>")
+                                .withHeader(HttpHeaders.Names.CONTENT_TYPE, "application/xml")
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertNull(response.getReceivedResponseContext().getResponseBody());
+        Assert.assertEquals(HttpResponseStatus.ACCEPTED, response.getReceivedResponseContext().getResponseStatus());
+    }
+
+    @Test
+    public void testMissingHeaders() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath(processingPath)
+                                .withMethod(HttpMethod.POST).withBody(xmlBody)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertNull(response.getReceivedResponseContext().getResponseBody());
+        Assert.assertEquals(HttpResponseStatus.ACCEPTED, response.getReceivedResponseContext().getResponseStatus());
+
+    }
+
+    @Test
+    public void testInvalidHeader() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath(processingPath).withMethod(HttpMethod.POST)
+                                .withBody(plainFile)
+                                .withHeader(HttpHeaders.Names.CONTENT_TYPE, "application/xml")
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertNull(response.getReceivedResponseContext().getResponseBody());
+        Assert.assertEquals(HttpResponseStatus.ACCEPTED, response.getReceivedResponseContext().getResponseStatus());
+    }
+
+    @Test @Ignore("Because the code is not written properly")
+    public void testConnectionDropWhileReading() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort())).withEnableReadingConnectionDrop()
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath(path)
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertNull(response);
+    }
 }
