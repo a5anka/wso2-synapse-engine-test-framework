bba004e547b19bbe4f878479f3d5562a2b1d4b6c
Asanka Abeyweera <asankaab@wso2.com>
2017-10-20 13:11:54 +0530

    Add a small thread sleep if the buffer is not ready to save CPU

--- ServerAgent/src/main/java/org/wso2/synapse/test/framework/ServerLogReader.java
+++ ServerAgent/src/main/java/org/wso2/synapse/test/framework/ServerLogReader.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
 
 public class ServerLogReader implements Runnable {
     private String streamType;
@@ -73,6 +74,8 @@ public class ServerLogReader implements Runnable {
                         stringBuilder.append(s).append("\n");
                         log.error(s);
                     }
+                } else {
+                    TimeUnit.MILLISECONDS.sleep(200);
                 }
             }
         } catch (Exception ex) {
