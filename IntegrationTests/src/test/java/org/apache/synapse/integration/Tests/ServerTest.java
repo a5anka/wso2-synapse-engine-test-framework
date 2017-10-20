2030e0a85b59535f19c9858d3fffe460d4e59b1b
riyafa <riyafa.12@cse.mrt.ac.lk>
2017-10-20 13:53:56 +0530

    More tests and some server changes

--- IntegrationTests/src/test/java/org/apache/synapse/integration/Tests/ServerTest.java
+++ IntegrationTests/src/test/java/org/apache/synapse/integration/Tests/ServerTest.java
@@ -1,5 +1,317 @@
 package org.apache.synapse.integration.Tests;
 
-public class ServerTest {
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import org.apache.synapse.integration.BaseTest;
+import org.apache.synapse.integration.utils.TestUtils;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.wso2.carbon.protocol.emulator.dsl.Emulator;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientConfigBuilderContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientRequestBuilderContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientResponseBuilderContext;
+import org.wso2.carbon.protocol.emulator.http.client.contexts.HttpClientResponseProcessorContext;
 
+import java.io.File;
+import java.io.IOException;
+
+
+public class ServerTest extends BaseTest{
+    private File plainFile = new File("100KB.txt");
+    @Test
+    public void testLargePayload() throws IOException {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/large_payload")
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals(response.getReceivedResponseContext().getResponseBody(),
+                            TestUtils.getFileBody(new File("1MB.txt")));
+    }
+
+    @Test
+    public void testSlowResponse() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/slow_response")
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals("Slowly responding backend", response.getReceivedResponseContext().getResponseBody());
+    }
+
+    @Test
+    public void testReadingDelay() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/reading_delay")
+                                .withMethod(HttpMethod.POST)
+                                .withBody("A small body to avoid timeout in the synapse server")
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals("Slowly reading backend", response.getReceivedResponseContext().getResponseBody());
+    }
+
+    @Test
+    public void testWritingDelay() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/writing_delay")
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals("Slowly writing backend", response.getReceivedResponseContext().getResponseBody());
+    }
+
+    @Test
+    public void testKeepAlive() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/keep_alive")
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals("Keep alive", response.getReceivedResponseContext().getResponseBody());
+    }
+
+    @Test
+    public void testChunkingDisabled() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/chunking_disabled")
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertNull(response);
+    }
+
+    @Test
+    public void testChunkingDisabledSynapse() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/chunking_disabled_synapse")
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                                .withHeader(HttpHeaders.Names.CONTENT_TYPE, "text/plain")
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals("Chunking disabled", response.getReceivedResponseContext().getResponseBody());
+    }
+
+    @Test
+    public void testHttp10NotSupported() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/support_http10")
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals("The HTTP/1.1 is not supported because of the configurations\n",
+                            response.getReceivedResponseContext().getResponseBody());
+        Assert.assertEquals(response.getReceivedResponse().getStatus(), HttpResponseStatus.HTTP_VERSION_NOT_SUPPORTED);
+    }
+
+    @Test
+    public void testMalformedPayload() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/malformed_payload")
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertNull(response);
+    }
+
+    @Test
+    public void testRandomDrop() {
+        Emulator.getHttpEmulator().client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/random_drop")
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/normal_server")
+                                .withMethod(HttpMethod.POST).withBody(plainFile)
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertEquals(response.getReceivedResponseContext().getResponseBody(), "{\"glossary\":{\"title" +
+                "\":\"exampleglossary\",\"GlossDiv\":{\"title\":\"S\",\"GlossList\":{\"GlossEntry\":{\"ID\":\"SGML\"," +
+                "\"SortAs\":\"SGML\",\"GlossTerm\":\"StandardGeneralizedMarkupLanguage\",\"Acronym\":\"SGML\"," +
+                "\"Abbrev\":\"ISO8879:1986\",\"GlossDef\":{\"para\":\"Ameta-markuplanguage," +
+                "usedtocreatemarkuplanguagessuchasDocBook.\",\"GlossSeeAlso\":[\"GML\",\"XML\"]}," +
+                "\"GlossSee\":\"markup\"}}}}}");
+        Assert.assertEquals("application/json",
+                            response.getReceivedResponse().headers().get(HttpHeaders.Names.CONTENT_TYPE));
+    }
+
+    @Test
+    public void testMissingHeader() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/missing_header")
+                                .withMethod(HttpMethod.POST).withBody(new File("100KB.txt"))
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertNotEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                                       "<note>\n" +
+                                       "  <to>Tove</to>\n" +
+                                       "  <from>Jani</from>\n" +
+                                       "  <heading>Reminder</heading>\n" +
+                                       "  <body>Don't forget me this weekend!</body>\n" +
+                                       "</note>",
+                               response.getReceivedResponseContext().getResponseBody());
+        Assert.assertEquals("application/octet-stream; charset=UTF-8",
+                            response.getReceivedResponse().headers().get(HttpHeaders.Names.CONTENT_TYPE));
+    }
+
+    @Test
+    public void testReadingDrop() {
+        HttpClientResponseProcessorContext response = Emulator.getHttpEmulator()
+                .client()
+                .given(
+                        HttpClientConfigBuilderContext.configure()
+                                .host(getConfig().getSynapseServer().getHostname())
+                                .port(Integer.parseInt(getConfig().getSynapseServer().getPort()))
+                )
+                .when(
+                        HttpClientRequestBuilderContext.request().withPath("/services/reading_delay_server")
+                                .withMethod(HttpMethod.POST).withBody(new File("100KB.txt"))
+                )
+                .then(
+                        HttpClientResponseBuilderContext.response().assertionIgnore()
+                )
+                .operation()
+                .send();
+        Assert.assertNull(response);
+    }
+
+    @Override
+    protected String getSynapseConfig() throws IOException {
+        return TestUtils.getSynapseConfig("server.xml");
+
+    }
 }
